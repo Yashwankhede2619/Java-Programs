fb904d6a849cb6433b921f7d80c0337469b368ad
Yashwankhede2619 <yash226718@gmail.com>
2025-06-13 12:04:07 +0530

    method chaining

---
+++ FormDriver.java
@@ -0,0 +1,15 @@
+class FormDriver
+{
+	public static void main(String[] args) 
+	{
+		Form f1=new Form();
+		f1.displayForm();
+		
+		System.out.println("............................");
+		
+		Form f2=new Form("Yash",9373,22,"yash226718@gmail.com","Pune",123,"B+","Pune");
+		f2.displayForm();
+
+	}
+}
+	
